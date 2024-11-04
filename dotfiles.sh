5901dce6fb345452b151593a8d72a69aa5d60ce3
Patrick Bulteel (UK) <Patrick.Bulteel@virginmediao2.co.uk>
2024-11-04 12:18:21 +0000

    Added some colour and tux

--- dotfiles.sh
+++ dotfiles.sh
@@ -10,19 +10,41 @@
 
 COMMAND=$1
 
+set -euo pipefail
+IFS=$'\n\t'
+
+cat << 'EOF'
+      .--.
+     |o_o |  HI :)
+     |:_/ |
+    //   \ \
+   (|     | )
+  /'\_   _/`\
+  \___)=(___/
+EOF
+
+
+### COLOR ### (https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux)
+NC='\033[0m' # No Color
+# Bold
+BRED='\033[1;31m'    # Red
+BPURPLE='\033[1;35m' # Purple
+BYELLOW='\033[1;33m' # Yellow
+BCyan='\033[1;36m'   # Cyan
+
 case "${COMMAND}" in
   'restore')
-    echo 'Copying files over...'
-    cp ./{.bashrc,.aliases,.bash_profile,.bash_prompt,.bash-preexec.sh,.extra,.functions} \
+    echo -e '${BRED}Copying files over...${NC}'
+    echo cp ./{.bashrc,.aliases,.bash_profile,.bash_prompt,.bash-preexec.sh,.extra,.functions} \
     "${HOME}"/.
     ;;
   'backup')
-    echo 'Backing up files into git repo...'
-    cp "${HOME}"/{.bashrc,.aliases,.bash_profile,.bash_prompt,.bash-preexec.sh,.extra,.functions} \
+    echo -e 'Backing up files into git repo...'
+    echo cp "${HOME}"/{.bashrc,.aliases,.bash_profile,.bash_prompt,.bash-preexec.sh,.extra,.functions} \
         /.
     ;;
   *)
-    echo "$0: invalid option -- ${COMMAND}"
+    echo -e "${BYELLOW}$0: invalid option -- ${BRED}\"${COMMAND}\"${NC}"
     echo "Usage: $0 <command>"
     echo "  backup         Copies the files from \$HOME to your git repo"
     echo "  restore        Copies the files from your git repo into \$HOME"
